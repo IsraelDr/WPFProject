2b78e1cfedfb86648fec117fc6fc90d250156490
Israel <Israel@github.com>
2017-12-03 14:04:08 +0200

    New2

--- Ex1_BE/MyBE.cs
+++ Ex1_BE/MyBE.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace Ex1_BE
 {
-    public class Class1
+    public class MyBE
     {
     }
 }
--- Ex1_DAL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ex1_DAL
-{
-    public class Class1
-    {
-    }
-}
